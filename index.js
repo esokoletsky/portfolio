5d42b7bd9f4ef58fc4f038f009026ab80d2cb8d1
esokoletsky <Esokoletsky@gmail.com>
2019-03-08 00:05:02 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+
+
+                Eugene = Developer();
+
+                while (Eugene.alive()) {
+                    Eugene.sleep();
+                    continue;
+                    Eugene.eat();
+                    Eugene.practice();
+                    Eugene.work();
+                    Eugene.makeContributionToSociety();
+                    Eugene.beProductive();
+                    Eugene.doLiterallyAnything();
+                }
+
+
+
+
+
+                
